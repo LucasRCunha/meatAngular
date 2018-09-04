40139ae4be1ba264f59159336dcf62e042a353f2
LucasRCunha <lucasrcunha01@gmail.com>
2018-09-04 17:30:40 -0300

    Protegendo o Módulo de Compra com CanLoad

--- meat-app-starter/src/app/security/login/login.service.ts
+++ meat-app-starter/src/app/security/login/login.service.ts
@@ -3,13 +3,15 @@ import {HttpClient} from "@angular/common/http"
 import { Observable } from "rxjs/Observable";
 import {MEAT_API} from '../../app.api'
 import { User } from "./user.model";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class LoginService{
 
     user: User
 
-    constructor(private http: HttpClient){}
+    constructor(private http: HttpClient,
+                private router: Router){}
 
     isLoggedIn(): boolean{
         return this.user !== undefined
@@ -17,6 +19,11 @@ export class LoginService{
 
     login(email: string, password: string):Observable<any>{
         return this.http.post<User>(`${MEAT_API}/login`, {email: email, password: password})
+                        .do(user => this.user = user)
+    }
+
+    handleLogin(path?: string){
+        this.router.navigate(['/login', path])
     }
 
 }
