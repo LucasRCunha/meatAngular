a41a13bc67adc104b7800045ac9bc36b79347510
LucasRCunha <lucasrcunha01@gmail.com>
2018-09-05 10:19:07 -0300

    Disparando o Login Manualmente

--- meat-app-starter/src/app/security/login/login.service.ts
+++ meat-app-starter/src/app/security/login/login.service.ts
@@ -3,15 +3,20 @@ import {HttpClient} from "@angular/common/http"
 import { Observable } from "rxjs/Observable";
 import {MEAT_API} from '../../app.api'
 import { User } from "./user.model";
-import { Router } from "@angular/router";
+import { Router, NavigationEnd } from "@angular/router";
+import 'rxjs/add/operator/filter'
 
 @Injectable()
 export class LoginService{
 
     user: User
+    lastUrl: string
 
     constructor(private http: HttpClient,
-                private router: Router){}
+                private router: Router){
+        this.router.events.filter(e => e instanceof NavigationEnd)
+                            .subscribe((e: NavigationEnd)=> this.lastUrl = e.url)
+    }
 
     isLoggedIn(): boolean{
         return this.user !== undefined
@@ -22,8 +27,13 @@ export class LoginService{
                         .do(user => this.user = user)
     }
 
-    handleLogin(path?: string){
+    logout(){
+        this.user=undefined
+    }
+
+    handleLogin(path: string = this.lastUrl){
         this.router.navigate(['/login', btoa(path)])
     }
+    
 
 }
